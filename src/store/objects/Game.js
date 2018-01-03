b6b9dd583588923da31c89e8e505219c879397b2
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2018-01-03 15:47:32 +0100

    UPDATE - add default objects

---
+++ src/store/objects/Game.js
@@ -0,0 +1,30 @@
+class Game {
+  constructor(id, player1, player2) {
+    this.id = id;
+    this.player1 = player1;
+    this.player2 = player2;
+    this.scorePlayer1 = 0;
+    this.scorePlayer2 = 0;
+  }
+
+  setScore(player, score) {
+    if (score >= 0) {
+      if (this.player1.id === player.id) {
+        this.scorePlayer1 = score;
+      } else if (this.player2.id === player.id) {
+        this.scorePlayer2 = score;
+      }
+    }
+  }
+
+  getWinner() {
+    if (this.scorePlayer1 > this.scorePlayer2) {
+      return this.player1;
+    } else if (this.scorePlayer2 > this.scorePlayer1) {
+      return this.player2;
+    }
+    return null;
+  }
+}
+
+export default Game;
---
+++ src/store/objects/League.js
@@ -0,0 +1,13 @@
+class League {
+  constructor(id) {
+    this.id = id;
+    this.startingDate = new Date();
+    this.games = [];
+  }
+
+  getTopPlayers() {
+    return [];
+  }
+}
+
+export default League;
