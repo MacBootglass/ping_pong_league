035b296dc6b544de0c0013a2301fe5c51f785466
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2018-01-03 17:47:04 +0100

    UPDATE - first step get top players

--- src/store/objects/League.js
+++ src/store/objects/League.js
@@ -1,3 +1,34 @@
+import Game from './Game';
+
+const calcPlayersStats = (games) => {
+  const playerList = [];
+  games.forEach((game) => {
+    if (playerList[game.player1]) {
+      playerList[game.player1].totalPlays += 1;
+    } else {
+      playerList[game.player1] = {
+        totalPlays: 0,
+        totalWins: 0,
+      };
+    }
+
+    if (playerList[game.player2]) {
+      playerList[game.player2].totalPlays += 1;
+    } else {
+      playerList[game.player2] = {
+        totalPlays: 0,
+        totalWins: 0,
+      };
+    }
+
+    playerList[game.getWinner()].totalWins += 1;
+  });
+};
+
+const calcRatio = (player) => {
+  return player.totalWins / player.totalPlays;
+};
+
 class League {
   constructor(id, name) {
     this.id = id;
@@ -6,8 +37,14 @@ class League {
     this.games = [];
   }
 
+  newGame(id, player1, player2) {
+    const game = new Game(id, player1, player2);
+    this.games.push(game);
+  }
+
   getTopPlayers() {
-    return [];
+    const playersStats = calcPlayersStats(this.games);
+    console.log(playersStats);
   }
 }
 
