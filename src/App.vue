4b28ad2489d0bc4406e2496d1a9371b4c0ede4fc
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2018-01-03 16:15:22 +0100

    UPDATE - first draft layer list component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <navbar title="Dashboard"/>
+  <div class="app">
+    <div class="page">
+      <navbar title="Dashboard"/>
+    </div>
+    <player-list/>
+  </div>
 </template>
 
 <script>
 import store from './store';
 import Navbar from './components/Navbar.vue';
+import PlayerList from './components/PlayerList.vue';
 
 export default {
   name: 'app',
@@ -13,9 +19,19 @@ export default {
   },
   components: {
     Navbar,
+    PlayerList,
   }
 }
 </script>
 
 <style lang="scss">
+  .app {
+    height: 100%;
+    width: 100%;
+    display: flex;
+
+    .page {
+      flex: 1;
+    }
+  }
 </style>
---
+++ src/components/PlayerList.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="player-list bg-dark">
+    <span class="title">Player list</span>
+  </section>
+</template>
+
+<script>
+import store from '../store';
+
+export default {
+  name: 'player-list',
+  store,
+  props: [
+  ],
+}
+</script>
+<style lang="scss" scoped>
+  @import '~spectre.css/src/variables';
+
+  .title {
+    font-size: 1.5rem;
+  }
+
+  .player-list {
+    border-left-color: $primary-color;
+    border-left-width: 1px;
+    border-left-style: solid;
+  }
+</style>
